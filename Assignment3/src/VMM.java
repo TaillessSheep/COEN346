7d6997f0f0796cd1d7688ba91550ba2b47162fb9
RookieDaniel <lisunhe@hotmail.com>
2019-04-03 17:45:17 -0400

    a little vmm

--- Assignment3/src/VMM.java
+++ Assignment3/src/VMM.java
@@ -1,4 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class VMM {
+	
+	void readSize() {
+	String fileName = "memconfig.txt";
+	String instructions = null;
+	int PageSize;
+	String size;
+	
+	try {
+		FileReader fileReader =  new FileReader(fileName);
+		BufferedReader bufferedReader =  new BufferedReader(fileReader);
+		size = bufferedReader.readLine();
+		PageSize = Integer.parseInt(size);
+		bufferedReader.close();
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		
+	}
+	public void Store(String id, int value) {
+		
+		
+	}
 
 }
